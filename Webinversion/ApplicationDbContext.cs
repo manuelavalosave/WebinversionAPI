9b7879f14faf2864c21d4342cc3bedd3e87d0b0a
Manuel Avalos <60284404+manuelavalosave@users.noreply.github.com>
2022-06-09 18:44:18 -0500

    Add project files.

---
+++ Webinversion/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Webinversion
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
