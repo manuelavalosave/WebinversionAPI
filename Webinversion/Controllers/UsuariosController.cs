ac3ee94857bbe08fa776698231ad39ecf207f73a
Manuel Avalos <60284404+manuelavalosave@users.noreply.github.com>
2022-06-12 05:34:22 -0500

    api funciondo al 100

--- Webinversion/Controllers/UsuariosController.cs
+++ Webinversion/Controllers/UsuariosController.cs
@@ -61,7 +61,7 @@ namespace Webinversion.Controllers
         [HttpDelete("{id:int}")]
         public async Task<ActionResult<Usuario>> Delete(int id)
         {
-            var datonversor = context.Inversionista.FromSqlInterpolated($"EXEC EliminarRegistros @id={id}").AsAsyncEnumerable();
+            var datonversor = await context.Database.ExecuteSqlInterpolatedAsync($"EXEC EliminarRegistros @id={id}");
 
 
             return NoContent();
@@ -72,12 +72,48 @@ namespace Webinversion.Controllers
         public async Task<ActionResult<Usuario>> PostBeneficiario(int id_inv, int porcentaje, Usuario usuario)
         {
 
-          
-            var datonversor = context.Inversionista.FromSqlInterpolated($"EXEC Benifesario_Insertar @id_inv={id_inv}, @porcentaje = {porcentaje}, @nombre={usuario.Name}, @segundo_nombre={usuario.Second_name},@apellido_p = {usuario.Last_name},@apellido_m = {usuario.Las_name_mother},@fecha_nacimiento = {usuario.Date_of_Birth}, @Curp = {usuario.CURP}, @SSN ={usuario.SSN},@Nacionalidad = {usuario.Nationality}, @id").AsAsyncEnumerable();
+            var datonversor = context.Inversionista.FromSqlInterpolated($"EXEC Inversor_Registros @id={id_inv}").AsAsyncEnumerable();
 
+            await foreach (var usuario2 in datonversor)
+            {
+                var demo = await context.Database.ExecuteSqlInterpolatedAsync($"EXEC Benifesario_Insertar @id_inv={id_inv}, @porcentaje = {porcentaje}, @nombre={usuario.Name}, @segundo_nombre={usuario.Second_name},@apellido_p = {usuario.Last_name},@apellido_m = {usuario.Las_name_mother},@fecha_nacimiento = {usuario.Date_of_Birth}, @Curp = {usuario.CURP}, @SSN ={usuario.SSN},@Nacionalidad = {usuario.Nationality}, @id=1");
+
+
+
+                return Ok("datos insertados");
+            }
+
+            return NotFound();
            
 
-            return Ok(datonversor);
+            
+
+            
+        }
+
+        [HttpPut("beneficiario/{id_inv:int}/{id_benifi:int}")]
+        public async Task<ActionResult<Usuario>> PutBeneficiario(int id_inv,int id_benifi, int porcentaje_u, Usuario usuario)
+        {
+           var editarA = await context.Database.ExecuteSqlInterpolatedAsync($@"EXEC 
+                                        Inversor_modificar
+                                         @nombre={usuario.Name}, @segundo_nombre={usuario.Second_name},@apellido_p = {usuario.Last_name},@apellido_m = {usuario.Las_name_mother},@fecha_nacimiento = {usuario.Date_of_Birth}, @numero_inv = {usuario.Investor_Number}, @Curp = {usuario.CURP}, @SSN ={usuario.SSN},@Nacionalidad = {usuario.Nationality}, @id={id_benifi}");
+
+            var porcentaje = await context.Database.ExecuteSqlInterpolatedAsync($@"EXEC 
+                                        Beneficiario_modificar_porcentaje
+                                         @id_beni={id_benifi}, @porcentaje_act={porcentaje_u}");
+
+            return Ok("modificado");
+
+        }
+
+        [HttpDelete("beneficiario/{id_inv:int}/{id_benifi:int}/")]
+        public async Task<ActionResult<Usuario>> Delete(int id_inv, int id_benifi)
+        {
+            var eliminarDatosBeni = await context.Database.ExecuteSqlInterpolatedAsync($"EXEC Beneficiario_elminar @id_TABLA_benif={id_benifi}");
+            var datonversor = await context.Database.ExecuteSqlInterpolatedAsync($"EXEC EliminarRegistros @id={id_benifi}");
+
+            return NoContent();
+
         }
     }
 }
